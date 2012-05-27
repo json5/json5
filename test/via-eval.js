9e443074c01d29da32c8d07fd5b9cbef457a2242
Aseem Kishore <aseem.kishore@gmail.com>
2012-05-27 17:24:43 -0400

    Rename test code and cases directory.
    Now that these are no longer simple "examples", but true test cases.

--- test/via-eval.js
+++ test/via-eval.js
@@ -3,14 +3,15 @@ var FS = require('fs');
 var JSON5 = require('..');
 var Path = require('path');
 
-// Test JSON5.parse() with each file under ./examples by comparing its output
-// to eval(). It should eval() fine since JSON5 is a pure subset of ES5.
+// Test JSON5.parse() with each test case under /cases by comparing its output
+// with eval(). It should eval() fine since JSON5 is a pure subset of ES5.
+// Remember to wrap in parentheses before eval()'ing since {...} is ambiguous.
 
 // TODO More test cases, and ones that test specific features and edge cases.
 // Mozilla's test cases are a great inspiration and reference here:
 // http://mxr.mozilla.org/mozilla-central/source/js/src/tests/ecma_5/JSON/
 
-var dirPath = Path.resolve(__dirname, 'examples');
+var dirPath = Path.resolve(__dirname, 'cases');
 var files = FS.readdirSync(dirPath);
 
 function createTest(fileName) {

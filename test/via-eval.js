d7a8ea03de814b91617391938dde155978838b40
Aseem Kishore <aseem.kishore@gmail.com>
2012-05-27 19:43:11 -0400

    Fix and improve naming of comment test cases.
    Turns out comments following top-level values are fine -- we just need to make
    sure we account for them when we eval().

--- test/via-eval.js
+++ test/via-eval.js
@@ -8,7 +8,8 @@ var Path = require('path');
 // pure subset of ES5, and the JSON5.parse() output should match eval()'s. If
 // the input *isn't* valid, eval() will throw an error, and JSON5.parse()
 // should too. Remember to wrap the input in parentheses before eval()'ing,
-// since {...} is ambiguous in JavaScript.
+// since {...} is ambiguous in JavaScript. Also ensure the parentheses are on
+// lines of their own, to support inline comments.
 
 // TODO More test cases, and ones that test specific features and edge cases.
 // Mozilla's test cases are a great inspiration and reference here:
@@ -29,7 +30,7 @@ function createTest(fileName) {
         var objExp, valid = false;
 
         try {
-            objExp = eval('(' + str + ')');
+            objExp = eval('(\n' + str + '\n)');
             valid = true;
         } catch (err) {}
 

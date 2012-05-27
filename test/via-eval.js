bf8d1c950f967799848149b8d45a7b1375066108
Aseem Kishore <aseem.kishore@gmail.com>
2012-05-27 17:39:24 -0400

    Add negative (failure) test cases too.
    If eval() rejects them, JSON5.parse() should too.
    
    In the process, discovered that while {,} is invalid, [,] is perfectly fine!
    That's going to make the implementation tricky. =P
    
    Also discovered that two of the existing test cases (comments alone, as the
    top-level value) were invalid to begin with. =D

--- test/via-eval.js
+++ test/via-eval.js
@@ -4,8 +4,11 @@ var JSON5 = require('..');
 var Path = require('path');
 
 // Test JSON5.parse() with each test case under /cases by comparing its output
-// with eval(). It should eval() fine since JSON5 is a pure subset of ES5.
-// Remember to wrap in parentheses before eval()'ing since {...} is ambiguous.
+// with eval(). If the input is valid, it should eval() fine since JSON5 is a
+// pure subset of ES5, and the JSON5.parse() output should match eval()'s. If
+// the input *isn't* valid, eval() will throw an error, and JSON5.parse()
+// should too. Remember to wrap the input in parentheses before eval()'ing,
+// since {...} is ambiguous in JavaScript.
 
 // TODO More test cases, and ones that test specific features and edge cases.
 // Mozilla's test cases are a great inspiration and reference here:
@@ -18,11 +21,22 @@ function createTest(fileName) {
     var filePath = Path.join(dirPath, fileName);
     var str = FS.readFileSync(filePath, 'utf8');
 
-    exports[fileName] = function () {
-        var objExp = eval('(' + str + ')');
-        var objAct = JSON5.parse(str);
+    function parse() {
+        return JSON5.parse(str);
+    }
 
-        assert.deepEqual(objAct, objExp);
+    exports[fileName] = function test() {
+        var objAct, objExp;
+
+        try {
+            objExp = eval('(' + str + ')');
+        } catch (err) {}
+
+        if (objExp) {
+            assert.deepEqual(parse(), objExp);
+        } else {
+            assert.throws(parse);
+        }
     };
 }
 

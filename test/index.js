9f0cd5a379130270d16e43c989dcbf5417bbf790
Aseem Kishore <aseem.kishore@gmail.com>
2012-05-27 16:20:12 -0400

    Add a regular JSON testcase — npm's package.json!

--- test/index.js
+++ test/index.js
@@ -3,14 +3,24 @@ var FS = require('fs');
 var JSON5 = require('..');
 var Path = require('path');
 
-// Load the readme example JSON and compare it using JSON5 and eval().
-// It should eval() fine, and the values from both should be the same.
+// Test JSON5.parse() with each file under ./examples by comparing its output
+// to eval(). It should eval() fine since JSON5 is a pure subset of ES5.
 
-var path = Path.resolve(__dirname, 'example.json5');
-var str = FS.readFileSync(path, 'utf8');
+// TODO More test cases, and ones that test specific features and edge cases.
+// Mozilla's test cases are a great inspiration and reference here:
+// http://mxr.mozilla.org/mozilla-central/source/js/src/tests/ecma_5/JSON/
 
-var objExp = eval('(' + str + ')');
-var objAct = JSON5.parse(str);
+var dirPath = Path.resolve(__dirname, 'examples');
+var files = FS.readdirSync(dirPath);
 
-assert.deepEqual(objAct, objExp);
+for (var i = 0; i < files.length; i++) {
+    var filePath = Path.join(dirPath, files[i]);
+    var str = FS.readFileSync(filePath, 'utf8');
+
+    var objExp = eval('(' + str + ')');
+    var objAct = JSON5.parse(str);
+
+    assert.deepEqual(objAct, objExp);
+}
+
 console.log('Test passed!');

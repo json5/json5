ef5cd0e5f565d742efd7bbc1b089aaa8a0b20b6d
Max Nanasy <max.nanasy@gmail.com>
2012-06-20 22:37:31 -0700

    Improve jQuery ajax test.

--- test/test-jquery.ajax.json5.js
+++ test/test-jquery.ajax.json5.js
@@ -1,15 +1,13 @@
 function test() {
-    $.each(['misc/readme-example.json5', 'misc/empty.txt'], function (i, path) {
+    $.each(['misc/readme-example.json5', 'numbers/noctal-literal.js'], function (i, path) {
         $.ajax({
             url: 'parse-cases/' + path,
             dataType: 'json5',
             success: function (result) {
-                console && console.log && console.log(path, result);
-                consolas && consolas.log && consolas.log(path, result);
+                console.log({ path: path, result: result });
             },
-            error: function (error) {
-                console && console.error && console.error(path, error);
-                typeof consolas !== 'undefined' && consolas.log && consolas.log(path, result);
+            error: function (jqXhr, textStatus, thrownError) {
+                console.log({ path: path, jqXhr: jqXhr, textStatus: textStatus, thrownError: thrownError });
             }
         });
     });

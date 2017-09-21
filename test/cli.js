0da82702a946ffc6047552320305f9b04c2eaf90
Jordan Tucker <jordanbtucker@gmail.com>
2017-09-21 14:30:27 -0500

    Add CLI tests

---
+++ test/cli.js
@@ -0,0 +1,215 @@
+import assert from 'assert'
+import child from 'child_process'
+import fs from 'fs'
+import path from 'path'
+
+import packageJSON from '../package.json'
+
+const cliPath = path.resolve(__dirname, '../lib/cli.js')
+
+describe('CLI', () => {
+    it('converts JSON5 to JSON from stdin to stdout', done => {
+        const proc = child.spawn(process.execPath, [cliPath])
+        let output = ''
+        proc.stdout.on('data', data => {
+            output += data
+        })
+
+        proc.stdout.on('end', () => {
+            assert.strictEqual(output, '{"a":1,"b":2}')
+            done()
+        })
+
+        fs.createReadStream(path.resolve(__dirname, 'test.json5')).pipe(proc.stdin)
+    })
+
+    it('reads from the specified file', done => {
+        const proc = child.spawn(
+            process.execPath,
+            [
+                cliPath,
+                path.resolve(__dirname, 'test.json5'),
+            ]
+        )
+
+        let output = ''
+        proc.stdout.on('data', data => {
+            output += data
+        })
+
+        proc.stdout.on('end', () => {
+            assert.strictEqual(output, '{"a":1,"b":2}')
+            done()
+        })
+    })
+
+    it('indents output with the number of spaces specified', done => {
+        const proc = child.spawn(
+            process.execPath,
+            [
+                cliPath,
+                path.resolve(__dirname, 'test.json5'),
+                '-s',
+                '4',
+            ]
+        )
+
+        let output = ''
+        proc.stdout.on('data', data => {
+            output += data
+        })
+
+        proc.stdout.on('end', () => {
+            assert.strictEqual(output, '{\n    "a": 1,\n    "b": 2\n}')
+            done()
+        })
+    })
+
+    it('indents output with tabs when specified', done => {
+        const proc = child.spawn(
+            process.execPath,
+            [
+                cliPath,
+                path.resolve(__dirname, 'test.json5'),
+                '-s',
+                't',
+            ]
+        )
+
+        let output = ''
+        proc.stdout.on('data', data => {
+            output += data
+        })
+
+        proc.stdout.on('end', () => {
+            assert.strictEqual(output, '{\n\t"a": 1,\n\t"b": 2\n}')
+            done()
+        })
+    })
+
+    it('outputs to the specified file', done => {
+        const proc = child.spawn(
+            process.execPath,
+            [
+                cliPath,
+                path.resolve(__dirname, 'test.json5'),
+                '-o',
+                path.resolve(__dirname, 'output.json'),
+            ]
+        )
+
+        proc.on('exit', () => {
+            assert.strictEqual(
+                fs.readFileSync(
+                    path.resolve(__dirname, 'output.json'),
+                    'utf8'
+                ),
+                '{"a":1,"b":2}'
+            )
+            done()
+        })
+
+        after(() => {
+            try {
+                fs.unlinkSync(path.resolve(__dirname, 'output.json'))
+            } catch (err) {}
+        })
+    })
+
+    it('validates valid JSON5 files', done => {
+        const proc = child.spawn(
+            process.execPath,
+            [
+                cliPath,
+                path.resolve(__dirname, 'test.json5'),
+                '-v',
+            ]
+        )
+
+        proc.on('exit', code => {
+            assert.strictEqual(code, 0)
+            done()
+        })
+    })
+
+    it('validates invalid JSON5 files', done => {
+        const proc = child.spawn(
+            process.execPath,
+            [
+                cliPath,
+                path.resolve(__dirname, 'invalid.json5'),
+                '-v',
+            ]
+        )
+
+        let error = ''
+        proc.stderr.on('data', data => {
+            error += data
+        })
+
+        proc.stderr.on('end', () => {
+            assert.strictEqual(error, "JSON5: invalid character 'a' at 1:1\n")
+        })
+
+        proc.on('exit', code => {
+            assert.strictEqual(code, 1)
+            done()
+        })
+    })
+
+    it('outputs the version number when specified', done => {
+        const proc = child.spawn(process.execPath, [cliPath, '-V'])
+
+        let output = ''
+        proc.stdout.on('data', data => {
+            output += data
+        })
+
+        proc.stdout.on('end', () => {
+            assert.strictEqual(output, packageJSON.version + '\n')
+            done()
+        })
+    })
+
+    it('outputs usage information when specified', done => {
+        const proc = child.spawn(process.execPath, [cliPath, '-h'])
+
+        let output = ''
+        proc.stdout.on('data', data => {
+            output += data
+        })
+
+        proc.stdout.on('end', () => {
+            assert(/Usage/.test(output))
+            done()
+        })
+    })
+
+    it('is backward compatible with v0.5.1', done => {
+        const proc = child.spawn(
+            process.execPath,
+            [
+                cliPath,
+                '-c',
+                path.resolve(__dirname, 'test.json5'),
+            ]
+        )
+
+        proc.on('exit', () => {
+            assert.strictEqual(
+                fs.readFileSync(
+                    path.resolve(__dirname, 'test.json'),
+                    'utf8'
+                ),
+                '{"a":1,"b":2}'
+            )
+            done()
+        })
+
+        after(() => {
+            try {
+                fs.unlinkSync(path.resolve(__dirname, 'test.json'))
+            } catch (err) {}
+        })
+    })
+})

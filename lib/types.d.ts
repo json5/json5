78030b3c96cb0fec1428698b1735f6c0304dacac
George Pickering <29524044+geopic@users.noreply.github.com>
2020-08-13 19:13:40 +0100

    Amend types declaration file (add StringifyOptions type)

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,4 +1,21 @@
 declare module 'json5' {
+  type StringifyOptions = Partial<{
+    /**
+     * A function that alters the behavior of the stringification process, or an array of String and Number objects that serve as a whitelist for selecting/filtering the properties of the value object to be included in the JSON5 string. If this value is null or not provided, all properties of the object are included in the resulting JSON5 string.
+     */
+    replacer: (this: any, key: string, value: any) => any;
+
+    /**
+     * A String or Number object that's used to insert white space into the output JSON5 string for readability purposes. If this is a Number, it indicates the number of space characters to use as white space; this number is capped at 10 (if it is greater, the value is just 10). Values less than 1 indicate that no space should be used. If this is a String, the string (or the first 10 characters of the string, if it's longer than that) is used as white space. If this parameter is not provided (or is null), no white space is used. If white space is used, trailing commas will be used in objects and arrays.
+     */
+    space: string | number;
+
+    /**
+     * A String representing the quote character to use when serializing strings.
+     */
+    quote: string;
+  }>
+
   /**
    * Parses a JSON5 string, constructing the JavaScript value or object described
    * by the string. An optional reviver function can be provided to perform a
@@ -31,9 +48,5 @@ declare module 'json5' {
    * 
    * `quote`: A String representing the quote character to use when serializing strings.
    */
-  export function stringify(value: any, options?: Partial<{
-    replacer: (this: any, key: string, value: any) => any;
-    space: string | number;
-    quote: string;
-  }>): string;
+  export function stringify(value: any, options?: StringifyOptions): string;
 }

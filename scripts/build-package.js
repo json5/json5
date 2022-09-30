60b5a786c0c838d0f869e055aae6e34e057cdd82
Jordan Tucker <jordanbtucker@gmail.com>
2022-09-30 18:31:15 -0500

    build: update package.json5 with staged package.json

--- scripts/build-package.js
+++ scripts/build-package.js
@@ -1,11 +1,39 @@
 #!/usr/bin/env node
 
+/**
+ * @file Updates `package.json5` to match `package.json`. If part or all of
+ * `package.json` is staged, then `package.json5` will be updated to match the
+ * staged version of `package.json` and then staged.
+ */
+
 const fs = require('fs')
-const path = require('path')
+const {simpleGit} = require('simple-git')
 const JSON5 = require('..')
-const pkg = require('../package.json')
 
-let pkg5 = '// This is a generated file. Do not edit.\n'
-pkg5 += pkg5 = JSON5.stringify(pkg, null, 2)
+async function main() {
+  let pkg
+  let isStaged = false
+  const git = simpleGit()
+  const stagedFiles = await git.diff(['--cached', '--name-only'])
+  if (/^package\.json$/m.test(stagedFiles)) {
+    const pkgJSON = await git.show([':package.json'])
+    pkg = JSON5.parse(pkgJSON)
+    isStaged = true
+  } else {
+    pkg = require('../package.json')
+  }
 
-fs.writeFileSync(path.resolve(__dirname, '..', 'package.json5'), pkg5)
+  const pkg5JSON5 =
+    '// This is a generated file. Do not edit.\n' +
+    JSON5.stringify(pkg, null, 2)
+  fs.writeFileSync(require.resolve('../package.json5'), pkg5JSON5)
+
+  if (isStaged) {
+    await git.add(['package.json5'])
+  }
+}
+
+main().catch(err => {
+  console.error(err)
+  process.exitCode = 1
+})

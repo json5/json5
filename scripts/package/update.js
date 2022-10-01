d52b7b6f44451baf5c0c6b6c0a479a44d9164641
Jordan Tucker <jordanbtucker@gmail.com>
2022-10-01 02:03:32 -0500

    build: add package.json5 on version

--- scripts/package/update.js
+++ scripts/package/update.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
 /**
- * @file Updates `package.json5` to match `package.json`. If part or all of
- * `package.json` is staged, then `package.json5` will be updated to match the
- * staged version of `package.json` and then staged.
+ * @file Updates `package.json5` to match `package.json`.
+ *
+ * If part or all of `package.json` is staged, then `package.json5` will be
+ * updated and staged to match the staged version of `package.json`.
+ *
+ * If the first command-line argument is `-a` or `--add`, then `package.json5`
+ * will be staged even if `package.json` is not staged.
  */
 
 const fs = require('fs')
@@ -11,14 +15,16 @@ const {simpleGit} = require('simple-git')
 const JSON5 = require('../..')
 
 async function main() {
+  const addArg = process.argv[2]
+  let shouldStage = addArg === '-a' || addArg === '--add'
+
   let pkg
-  let isStaged = false
   const git = simpleGit()
   const stagedFiles = await git.diff(['--cached', '--name-only'])
   if (/^package\.json$/m.test(stagedFiles)) {
     const pkgJSON = await git.show([':package.json'])
     pkg = JSON5.parse(pkgJSON)
-    isStaged = true
+    shouldStage = true
   } else {
     pkg = require('../../package.json')
   }
@@ -28,7 +34,7 @@ async function main() {
     JSON5.stringify(pkg, null, 2)
   fs.writeFileSync(require.resolve('../../package.json5'), pkg5JSON5)
 
-  if (isStaged) {
+  if (shouldStage) {
     await git.add(['package.json5'])
   }
 }
